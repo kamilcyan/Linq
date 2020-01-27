008e665d97df747a023de0f2e16d901c037259ce
kamilcyan <kamil.lucjanek@wp.pl>
2020-01-27 21:49:37 +0100

    ser

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinQWithArrayOfStrings();
+
+            Console.ReadKey();
+        }
+
+        static void LinQWithArrayOfStrings()
+        {
+            var names = new string[] { "Michael", "Pam", "Jim", "Dwight", "Angela", "Kevin", "Toby" };
+
+            var query = names.Where(new Func<string, bool>(NoLongerThanFour));
+
+            foreach(string item in query)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static bool NoLongerThanFour(string name)
+        {
+            return name.Length > 4;
+        }
+    }
+}
